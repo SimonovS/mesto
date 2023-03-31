dbd41f7f16eb88a7a611c6cca0025f990adb143a
Sergey Simonov <sir.akter@yandex.ru>
2023-03-31 22:29:01 +0300

    fix: Замечания ревьювер v.2

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,32 @@
 
-const profileEdit = document.querySelector(".profile__edit");
-const openPopup = document.querySelector(".popup");
-const closePopup = openPopup.querySelector(".popup__close");
+const profileEdit = document.querySelector('.profile__edit');
+const openPopup = document.querySelector('.popup');
+const closePopup = openPopup.querySelector('.popup__close');
 
-const valueInputName = document.querySelector(".popup__input-name");
-const valueName = document.querySelector(".profile__info-name");
+const valueInputName = document.querySelector('.popup__input-name');
+const valueName = document.querySelector('.profile__info-name');
 
-const valueInputProfession = document.querySelector(".popup__input-profession");
-const valueProfession = document.querySelector(".profile__info-profession");
+const valueInputProfession = document.querySelector('.popup__input-profession');
+const valueProfession = document.querySelector('.profile__info-profession');
 
-const savePopup = openPopup.querySelector(".popup__input-button");
-const popupForm = openPopup.querySelector(".popup__form");
+const savePopup = openPopup.querySelector('.popup__input-button');
+const popupForm = openPopup.querySelector('.popup__form');
 
 function clickOpenPopup() {
-  openPopup.classList.add("popup_open");
+  openPopup.classList.add('popup_opened');
+
+  valueInputName.value = valueName.innerHTML;
+  valueInputProfession.value = valueProfession.innerHTML;
 }
 
 profileEdit.addEventListener('click', clickOpenPopup);
 
 
 function clickClosePopup() {
-  openPopup.classList.remove("popup_open");
+  openPopup.classList.remove('popup_opened');
 }
 
 closePopup.addEventListener('click', clickClosePopup);
-
-
-
-valueInputName.setAttribute("value", valueName.textContent);
-valueInputProfession.setAttribute("value", valueProfession.textContent);
 
 
 popupForm.addEventListener('submit', saveFormSubmit);
@@ -40,7 +38,6 @@ function saveFormSubmit(event) {
   const name = valueInputName.value
   valueName.innerHTML = name;
 
-  const profession = valueInputProfession.value
-  valueProfession.innerHTML = profession
-  openPopup.classList.remove("popup_open");
+  valueProfession.textContent = profession
+  openPopup.classList.remove('popup_close');
 }
